0474b98c9aa25a840672cacaf1fa58a177a6e6d1
Chris Zichko <chriszichkocoding@gmail.com>
2021-12-30 10:52:27 -0500

    Made the service worker pull from cache if the user loses internet.

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ self.addEventListener('activate', function (e) {
         return key.indexOf(APP_PREFIX);
       })
       cacheKeeplist.push(CACHE_NAME);
-      
+
       return Promise.all(keyList.map(function (key, i) {
         if (cacheKeeplist.indexOf(key) === -1) {
           console.log('deleting cache : ' + keyList[i]);
@@ -40,5 +40,23 @@ self.addEventListener('activate', function (e) {
         } 
       }));
     })
-  )
-})
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url);
+  e.respondWith (
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log('responding with cache : ' + e.request.url);
+        return request;
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url);
+        return fetch(e.request);
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  );
+});
